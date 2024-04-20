db3dee23f7dc89e98e489603f747ea8b19e41b37
MrVilka <157285886+MrVilka@users.noreply.github.com>
2024-04-20 18:50:11 +0500

    Update tempjs.js

--- tempjs.js
+++ tempjs.js
@@ -1,3 +1,5 @@
+// Для получения температуры и вывода ее
+
 const sCity = "Karagandy,KZ";
 let cityId = 0;
 const appid = "03371846c9b571d1a49cbb42d28b2da3";
@@ -23,30 +25,21 @@ function httpRequest(url) {
 window.onload = function() {
     window.setInterval(async function() {
         try {
-            const data = await httpRequest(`https://api.openweathermap.org/data/2.5/weather?q=${sCity}&units=metric&lang=ru&APPID=${appid}`);
-            // Время получить
-            let date = new Date();
-            let hou = date.getHours();
-            let min = date.getMinutes();
-            let sec = date.getSeconds();
-            // Если время меньше 0:
-            if(hou < 10){
-                hou = "0" + hou;
-            }
-            if(min < 10){
-                min = "0" + min;
-            }
-            if(sec < 10){
-                sec = "0" + sec;
-            }
-
+            const data = await httpRequest(`http://api.openweathermap.org/data/2.5/weather?q=${sCity}&units=metric&lang=ru&APPID=${appid}`);
+            
             info.textContent = "Температура: " + Math.round(data.main.temp) + "℃";
             feels_like.textContent = " * Ощущается как: " + Math.round(data.main.feels_like) + "℃";
             clouds.textContent = " * Состояние: " + (data.weather[0].description)[0].toUpperCase() + (data.weather[0].description).slice(1);
-            datee.textContent = hou + ":" + min + ":" + sec;
-            // document.getElementById('info').style.color="red";
         } catch (e) {
             console.log("Exception:", e);
         }
-    });
+    }, 1000);
+};
+
+//  Окно у уведомлений : 
+
+const divv = document.getElementById('hide');
+
+function toHide() {
+    divv.setAttribute('id', 'hidden')
 }
