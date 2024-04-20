59ce324e0bd2d1e0597d24fd385aeb5216c82d35
MrVilka <157285886+MrVilka@users.noreply.github.com>
2024-04-20 16:15:39 +0500

    Update tempjs.js

--- tempjs.js
+++ tempjs.js
@@ -24,7 +24,6 @@ window.onload = function() {
     window.setInterval(async function() {
         try {
             const data = await httpRequest(`https://api.openweathermap.org/data/2.5/weather?q=${sCity}&units=metric&lang=ru&APPID=${appid}`);
-
             // Время получить
             let date = new Date();
             let hou = date.getHours();
@@ -42,12 +41,12 @@ window.onload = function() {
             }
 
             info.textContent = "Температура: " + Math.round(data.main.temp) + "℃";
-            feels_like.textContent = " * Чувствуется как: " + Math.round(data.main.feels_like) + "℃";
+            feels_like.textContent = " * Ощущается как: " + Math.round(data.main.feels_like) + "℃";
             clouds.textContent = " * Состояние: " + (data.weather[0].description)[0].toUpperCase() + (data.weather[0].description).slice(1);
             datee.textContent = hou + ":" + min + ":" + sec;
             // document.getElementById('info').style.color="red";
         } catch (e) {
             console.log("Exception:", e);
         }
-    }, 1000);
-};
+    });
+}
